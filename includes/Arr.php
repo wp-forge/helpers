2f6881eaca3d093e568dab32a83e8a6f423d7cf4
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-10-24 14:46:39 +0100

    Fix nullable deprecated warnings

--- includes/Arr.php
+++ includes/Arr.php
@@ -195,7 +195,7 @@ class Arr {
 	 *
 	 * @return mixed
 	 */
-	public static function last( $array, callable $callback = null, $default = null ) {
+	public static function last( $array, ?callable $callback = null, $default = null ) {
 		if ( is_null( $callback ) ) {
 			return empty( $array ) ? $default : end( $array );
 		}
@@ -212,7 +212,7 @@ class Arr {
 	 *
 	 * @return mixed
 	 */
-	public static function first( $array, callable $callback = null, $default = null ) {
+	public static function first( $array, ?callable $callback = null, $default = null ) {
 		if ( is_null( $callback ) ) {
 			if ( empty( $array ) ) {
 				return $default;
